e722be8d6589e61168751ff92b7f12fb8a796954
morimonchan <morimorimonchan.com@gmail.com>
2022-09-28 11:18:40 +0900

    販売手数料の設定完了

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,10 +13,10 @@ class Item < ApplicationRecord
   validates :cost_burden_id ,presence: true, numericality:  { other_than: 1 , message: "can't be blank"}
   validates :place_id       ,presence: true, numericality:  { other_than: 1 , message: "can't be blank"}
   validates :sent_day_id    ,presence: true, numericality:  { other_than: 1 , message: "can't be blank"}
-  validates :price          ,presence: true
+  validates_format_of :price, with: /\A[0-9]+\z/
+  validates_inclusion_of :price, in:300..9999999
+
   validates :image          ,presence: true
-
-
 
   belongs_to :user
   # has_one    :purchase_history
