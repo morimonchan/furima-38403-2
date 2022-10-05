90ceecc2406729c6f5ed4d76580a863ced9a5a99
morimonchan <morimorimonchan.com@gmail.com>
2022-10-05 23:21:37 +0900

    purchase_history_sentのテスト実装

--- spec/models/purchase_history_sent_spec.rb
+++ spec/models/purchase_history_sent_spec.rb
@@ -14,37 +14,62 @@ RSpec.describe PurchaseHistorySent, type: :model do
        it '必須項目が存在すれば登録できる' do
         expect(@purchase_history_sent).to be_valid
        end
-      
-       it '建物名が空でも登録できる' do
-       end
-  
       end
   
       context '商品購入ができないとき' do
        it '郵便番号が空では登録できない' do
+        @purchase_history_sent.code = ""
+        @purchase_history_sent.valid?
+        expect(@purchase_history_sent.errors.full_messages).to include "Code can't be blank"
        end
   
        it '郵便番号は、「7桁」の半角文字では登録できない' do
+        @purchase_history_sent.code = "1111111"
+        @purchase_history_sent.valid?
+        expect(@purchase_history_sent.errors.full_messages).to include "Code is invalid. Include hyphen(-)"
        end
     
-       it '都道府県が空では登録できない' do
+       it '都道府県が----では登録できない' do
+        @purchase_history_sent.place_id = '1'
+        @purchase_history_sent.valid?
+        expect(@purchase_history_sent.errors.full_messages).to include "Place can't be blank"
        end
   
        it '市区町村が空では登録できない' do
+        @purchase_history_sent.city = ""
+        @purchase_history_sent.valid?
+        expect(@purchase_history_sent.errors.full_messages).to include "City can't be blank"
        end
   
        it '番地が空では登録できない' do
+        @purchase_history_sent.street_address = ""
+        @purchase_history_sent.valid?
+        expect(@purchase_history_sent.errors.full_messages).to include "Street address can't be blank"
        end
   
        it '電話番号が空では登録できない' do
+        @purchase_history_sent.phone_number = ""
+        @purchase_history_sent.valid?
+        expect(@purchase_history_sent.errors.full_messages).to include "Phone number can't be blank", "Phone number is invalid. Include hyphen(-)"
        end
       
-       it '電話番号は、9桁以下の半角数値では登録できない' do
+       it '電話番号は9桁以下の半角数値では登録できない' do
+        @purchase_history_sent.phone_number = "000111222"
+        @purchase_history_sent.valid?
+        expect(@purchase_history_sent.errors.full_messages).to include "Phone number is invalid. Include hyphen(-)"
        end
   
-       it '電話番号は、12桁以上の半角数値では登録できない' do
+       it '電話番号は12桁以上の半角数値では登録できない' do
+        @purchase_history_sent.phone_number = "000111222333"
+        @purchase_history_sent.valid?
+        expect(@purchase_history_sent.errors.full_messages).to include "Phone number is invalid. Include hyphen(-)"
        end
-  
+       
+       it '電話番号はハイフンがあると登録できない' do
+        @purchase_history_sent.phone_number = "000-1111-2222"
+        @purchase_history_sent.valid?
+        expect(@purchase_history_sent.errors.full_messages).to include "Phone number is invalid. Include hyphen(-)"
+       end
       end
     end
 end
